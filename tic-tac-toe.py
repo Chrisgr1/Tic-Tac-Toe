9d1fbfd07908f48d31f7d8a16773054289df5316
Chris <chris.gr.skinner@gmail.com>
2022-05-12 22:04:23 +0100

    brainstorm

---
+++ tic-tac-toe.py
@@ -0,0 +1,18 @@
+# board
+#display board
+
+
+# game 
+# input
+# check if win
+        # row
+        # column
+        # diagonal
+
+# choose next move
+# check if win
+        # row
+        # column
+        # diagonal
+
+# set winner
